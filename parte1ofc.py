74bea8ea0c87f091e3749eb65089ffb9c9704768
Daniel Rocha Franca <112904598+Dan007p4@users.noreply.github.com>
2023-05-24 00:15:56 -0300

    Update parte1ofc.py

--- parte1ofc.py
+++ parte1ofc.py
@@ -15,8 +15,8 @@ import matplotlib.pyplot as plt
 
 connection = mysql.connector.connect(
     host="aws.connect.psdb.cloud",
-    user="woespsre4lj6dp0r322k",
-    passwd="pscale_pw_JQDoNmNjU3nfdsdDuF9pJ4zLQ9CzieSFSIzEbcF5fwJ",
+    user="wu7ngppthyjxmkhztjgj",
+    passwd="pscale_pw_FPMpMLrVnUQYcB12T3qniEFGATaaZPGxhBmj9hHlp3l",
     db="database",
     ssl_ca="cacert-2023-01-10.pem"
     # ssl={
